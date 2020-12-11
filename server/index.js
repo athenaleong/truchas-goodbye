fc63edc1d0cf34f42b22d66c929c9d4d2a2635af
Athena Leong <athenaleong619@gmail.com>
2020-12-11 10:50:18 +0800

    minor

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.post('/uploadImage', urlParser, async function(req, res) {
 })
 
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use('/test', express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
