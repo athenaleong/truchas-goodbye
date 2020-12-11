afbe8c48e1d78cf40c2c1be546d41e6f81560944
Athena Leong <athenaleong619@gmail.com>
2020-12-11 10:42:26 +0800

    minor

--- server/index.js
+++ server/index.js
@@ -41,12 +41,12 @@ app.post('/uploadImage', urlParser, async function(req, res) {
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port);
-console.log("Listening on port: " + port);
+console.log("Listening on port : " + port);
 console.log(__dirname);
 
 
