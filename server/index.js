0ce0a71bce83be881b567229fcb00f2aa000a23b
Athena Leong <athenaleong619@gmail.com>
2020-12-11 09:39:31 +0800

    Added .. to server side

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const {MongoClient, Grid} = require('mongodb');
 const urlParser = bodyParser.urlencoded({extended: true});
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
