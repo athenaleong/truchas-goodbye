4927260afe5a07df439b828296a38cd2904485c6
Athena Leong <athenaleong619@gmail.com>
2021-01-01 21:42:03 +0800

    minor

--- server/index.js
+++ server/index.js
@@ -217,10 +217,10 @@ app.get('/getAlbumContent', async function (req, res) {
 //same origin 
 if (process.env.NODE_ENV === 'production') {
 
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "..", "client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, ".." , "client/build", "index.html"));
   });
 }
 
